8266fda2b377bede4064c7d33870e26f7e1a0f9f
nils-sw <nils.selvidge@gmail.com>
2025-09-15 21:53:58 +0200

    Adding chunk mesh shading functionality

--- chunk_mesh.h
+++ chunk_mesh.h
@@ -1,36 +1,31 @@
 #ifndef SIGCRAFT_CHUNK_MESH_H
 #define SIGCRAFT_CHUNK_MESH_H
 
-#include "world.h"
-
 #include "imr/imr.h"
+#include "nasl/nasl.h"
 
 #include <cstddef>
 
 struct ChunkNeighbors {
-    std::shared_ptr<Chunk> neighbours[3][3];
-};
-
-struct ChunkNeighborsUnsafe {
-    ChunkData* neighbours[3][3];
+    const ChunkData* neighbours[3][3];
 };
 
 struct ChunkMesh {
     std::unique_ptr<imr::Buffer> buf;
     size_t num_verts;
 
-    ChunkMesh(imr::Device&, std::mutex& device_mutex, ChunkNeighbors& n);
+    ChunkMesh(imr::Device&, ChunkNeighbors& n);
 
     struct Vertex {
-        int16_t vx, vy, vz;
-        uint8_t tt;
-        uint8_t ss;
-        uint8_t nnx, nny, nnz;
-        uint8_t pad;
-        uint8_t br, bg, bb, pad2;
+        int32_t vx, vy, vz;
+        uint32_t tt;
+        uint32_t ss;
+        uint32_t nnx, nny, nnz;
+        // uint32_t pad;
+        uint32_t br, bg, bb;
     };
 
-    static_assert(sizeof(Vertex) == sizeof(uint8_t) * 16);
+    static_assert(sizeof(Vertex) == sizeof(uint32_t) * 11);
 };
 
 #endif
